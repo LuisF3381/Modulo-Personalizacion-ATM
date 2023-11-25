eefb28ca818879706e28f465fd0ddb4afbc02311
LuisF3381 <89862061+LuisF3381@users.noreply.github.com>
2023-11-25 08:24:05 -0500

    Update db.py

--- config/db.py
+++ config/db.py
@@ -2,9 +2,7 @@ import MySQLdb
 
 # Database configuration
 db_config = {
-    'host': 'tesis2-database.czwpv7w8kpxj.us-east-1.rds.amazonaws.com',
     'user': 'admin',
-    'passwd': 'LUISF3436',
     'db': 'usermodeldatabase',
     'port': 3306,
 }
